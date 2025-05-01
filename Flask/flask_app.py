c68dd5baf38fd8480585a34ec3fe875c1dfeb78e
maaaxinfinity <3206288040@qq.com>
2025-05-01 16:27:37 +0800

    debug

--- Flask/flask_app.py
+++ Flask/flask_app.py
@@ -30,7 +30,7 @@ from api_utils import (
 ensure_dirs()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "api.webui.limitee.cn"}}) # Allow specific origin
+CORS(app, resources={r"/*": {"origins": ["*.vercel.app", "*.limitee.cn"]}})
 
 # Configure logging to match api_utils
 log_format = '%s(asctime)s - %s(name)s - %s(levelname)s - %s(message)s'
